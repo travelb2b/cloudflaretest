abc52950fbe0fbe519377a3e3a54f02b437b6094
antonvelev <xml@travel-b2b.com>
2015-01-10 03:50:28 +0200

    Update app_module.js

--- public/js/app_module.js
+++ public/js/app_module.js
@@ -2,24 +2,24 @@ CloudFlare.define("travelb2btest",
     ["cloudflare/jquery1.7"],
     function($)
     {
-        function addBadge(count){
+        function addBadge(count) {
             var cdnPath = "//cloudflare.travel-b2b.com/test";
-            var div = $("<div>")
+            var div = $("<div>");
             $("<img>",{
                 src: cdnPath + "img/test.png"
             }).appendTo(div);
-            $("<span>", {"style": "position:relative; top:-1px; left:-1px;")
-                .text(count).appendTo(div)
-            div.appendTo("body")
+            $("<span>", {"style": "position:relative; top:-1px; left:-1px;"})
+                .text(count).appendTo(div);
+            div.appendTo("body");
         }
         $.getJSON(
             "http://cloudflare.travel-b2b.com/test?callback=?",
             {domain_id: config && config.domain_id},
             function(data){
                 if (this.data.count){
-                    addBadge(this.data.count)
+                    addBadge(this.data.count);
                 }
             }
         )
     }
-)
+);
